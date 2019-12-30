a92e7a05a9e0b42e35079d4f2ebc25bbfa547505
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-12-30 17:40:31 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 1) do
+
+  create_table "shows", force: :cascade do |t|
+    t.string "name"
+    t.string "network"
+    t.string "day"
+    t.integer "rating"
+  end
 
 end
